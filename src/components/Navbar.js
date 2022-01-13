e5a71b5d9c6e03a91ebdc0aa56c3bcad9f2b1e40
xhanalexander <xhanalexander@gmail.com>
2022-01-13 16:27:07 +0700

    v1.0.6

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
          <header className="flex justify-between">
             <Link to="../work" className="md:text-3xl flex text-2xl text-white md:text-left text-center font-semibold font-title" title="home"> {name}</Link>
 
-            <button className="md:hidden flex transition" type="button" onClick={()=>setstate(!show)}>
+            <button className="md:hidden transition" type="button" onClick={()=>setstate(!show)}>
                {
                   show?
                   <nav className="md:hidden mobile block transition inset-0 backdrop-filter backdrop-blur bg-black bg-opacity-75 z-10">
@@ -21,15 +21,15 @@ export default function Navbar() {
                      </nav>
                   </nav>:null
                }
-               <svg className="h-9 w-9 fill-current text-white z-20" viewBox="0 0 24 24" stroke="currentColor">
+               <svg className="h-9 w-9 fill-current text-white z-20 fixed" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                </svg>
             </button>
 
             <div className="md:flex hidden ">
-               <Link to="../portfolio" className="md:text-xl text-xl text-white font-base font-title pr-10 text-opacity-75 hover:text-opacity-100" title="Portfolio" activeClassName='text-green-400 text-opacity-100 '>Portfolio</Link>
-               <Link to="../about" className="md:text-xl text-xl text-white font-base font-title pr-10 text-opacity-75 hover:text-opacity-100" title="About" activeClassName='text-green-400 text-opacity-100 '>About</Link>
-               <Link to="../contact" className="md:text-xl text-xl text-white font-base font-title text-opacity-75 hover:text-opacity-100" title="Contact" activeClassName='text-green-400 text-opacity-100 '>Contact</Link>
+               <Link to="../portfolio" className="md:text-base text-xl text-white font-title uppercase pr-10 text-opacity-75 hover:text-opacity-100" title="Portfolio" activeClassName='text-green-400 text-opacity-100 '>Portfolio</Link>
+               <Link to="../about" className="md:text-base text-xl text-white font-title uppercase pr-10 text-opacity-75 hover:text-opacity-100" title="About" activeClassName='text-green-400 text-opacity-100 '>About</Link>
+               <Link to="../contact" className="md:text-base text-xl text-white font-title uppercase text-opacity-75 hover:text-opacity-100" title="Contact" activeClassName='text-green-400 text-opacity-100 '>Contact</Link>
             </div>
 
          </header>
