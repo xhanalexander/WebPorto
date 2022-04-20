6f0ec783d336393b61e7405ea85807f90f801301
xhanalexander <xhanalexander@gmail.com>
2022-04-21 03:19:10 +0700

    v1.2.6

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
          options: {
            // You can add multiple tracking ids and a pageview event will be fired for all of them.
            trackingIds: [
-             "GA-TRACKING_ID", // Google Analytics / GA
+             "G-MCFR32FNJJ", // Google Analytics / GA
            ],
            // This object is used for configuration specific to this plugin
            pluginConfig: {
